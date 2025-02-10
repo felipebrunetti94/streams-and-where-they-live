bf42dc6afed5f982c41a3b7003e3c48b71c58909
Felipe Brunetti <felipe.brunetti94@gmail.com>
2025-02-10 18:18:54 -0300

    ref: rearrange files

--- server/index.js
+++ server/index.js
@@ -2,28 +2,31 @@ import http from "node:http";
 import path from "node:path";
 import * as fs from "node:fs";
 
-const __DIRNAME = import.meta.dirname;
 const DEFAULT_PORT = 8080;
 const PORT = process.env.PORT || DEFAULT_PORT;
+
+const __dirname = import.meta.dirname;
+const CLIENT_FOLDER = path.join(__dirname, "../client");
+const DATA_FOLDER = path.join(__dirname, "../data");
 
 function onRequest(request, response) {
   console.log("request", request);
   if (request.method === "GET" && request.url === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html");
-    fs.createReadStream(path.join(__DIRNAME, "/public/index.html"))
+    fs.createReadStream(path.join(CLIENT_FOLDER, "index.html"))
       .pipe(response)
       .on("close", () => response.end());
   } else if (request.method === "GET" && request.url === "/streams") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "application/json");
-    fs.createReadStream(path.join(__DIRNAME, "/data.json"))
+    fs.createReadStream(path.join(DATA_FOLDER, "data.json"))
       .pipe(response)
       .on("close", () => response.end());
   } else {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html");
-    fs.createReadStream(path.join(__DIRNAME, "/public/not_found.html"))
+    fs.createReadStream(path.join(CLIENT_FOLDER, "not_found.html"))
       .pipe(response)
       .on("close", () => response.end());
   }
